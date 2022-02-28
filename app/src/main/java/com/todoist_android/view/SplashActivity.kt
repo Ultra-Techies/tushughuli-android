68636981fb9b9887dac8ebd0c35d09c70b5b0820
malcolmmaima <malcolmmaima@gmail.com>
2022-02-28 13:10:32 +0300

    setup login fragment

--- app/src/main/java/com/todoist_android/view/SplashActivity.kt
+++ app/src/main/java/com/todoist_android/view/SplashActivity.kt
@@ -10,6 +10,7 @@ import android.os.Bundle
 import android.util.Log
 import com.google.android.material.snackbar.Snackbar
 import com.todoist_android.databinding.ActivitySplashBinding
+import com.todoist_android.view.auth.AuthActivity
 import kotlinx.coroutines.*
 import java.util.concurrent.TimeUnit
 
@@ -31,7 +32,7 @@ class SplashActivity : AppCompatActivity() {
                 delay(TimeUnit.SECONDS.toMillis(2))
                 withContext(Dispatchers.Main) {
                     Log.i("TAG", "this will be called after 2 seconds")
-                    startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+                    startActivity(Intent(this@SplashActivity, AuthActivity::class.java))
                     finish()
                 }
             }
---
+++ app/src/main/java/com/todoist_android/view/auth/AuthActivity.kt
@@ -0,0 +1,15 @@
+package com.todoist_android.view.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.todoist_android.databinding.ActivityAuthBinding
+
+class AuthActivity : AppCompatActivity() {
+    lateinit var binding: ActivityAuthBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityAuthBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
