310efed1a1f3bf82ed040f6b152456412c79ba28
Njumbi <wambuinjumbi@gmail.com>
2022-03-01 09:59:18 +0300

    feature: dependency injection set up

--- app/src/main/java/com/todoist_android/view/Utils.kt
+++ app/src/main/java/com/todoist_android/view/Utils.kt
@@ -1,5 +1,8 @@
 package com.todoist_android.view
 
+
+const val BASE_URL = "https://621ce943768a4e1020b93731.mockapi.io/api/v1/"
+
 private const val emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+"
 
 fun validateEmail(email: String): Boolean {
