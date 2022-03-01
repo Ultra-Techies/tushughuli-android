56710a19bd08d941aebe52b742db821bd32377cf
Njumbi <wambuinjumbi@gmail.com>
2022-03-01 09:14:55 +0300

    refactor: code clean up

--- app/src/main/java/com/todoist_android/ui/Utils.kt
+++
@@ -1,8 +0,0 @@
-package com.todoist_android.view
-
-private const val emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+"
-
-fun validateEmail(email: String): Boolean {
-    return email.matches(emailPattern.toRegex())
-}
-
