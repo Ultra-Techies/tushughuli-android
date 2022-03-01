96bdb5f590dd9ab8291c0f6a5c949643bb4c547a
Njumbi <wambuinjumbi@gmail.com>
2022-03-01 10:53:38 +0300

    feat: setup dependency injection

--- app/src/main/java/com/todoist_android/ui/auth/SignupFragment.kt
+++ app/src/main/java/com/todoist_android/ui/auth/SignupFragment.kt
@@ -1,8 +1,9 @@
 package com.todoist_android.ui.auth
 
 import androidx.fragment.app.Fragment
+import dagger.hilt.android.AndroidEntryPoint
 
-
+@AndroidEntryPoint
 class SignupFragment : Fragment() {
 
 }
