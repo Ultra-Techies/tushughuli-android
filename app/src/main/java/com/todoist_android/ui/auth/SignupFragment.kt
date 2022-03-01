0f08dddc9ae360a367830e1ddf1f4f012e62e943
malcolm maima <malcolmmaima@gmail.com>
2022-03-01 15:54:29 +0300

    SignupFragment empty causing build failure

--- app/src/main/java/com/todoist_android/ui/auth/SignupFragment.kt
+++ app/src/main/java/com/todoist_android/ui/auth/SignupFragment.kt
@@ -1,9 +1,27 @@
 package com.todoist_android.ui.auth
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.todoist_android.databinding.FragmentSignupBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class SignupFragment : Fragment() {
+    private lateinit var binding: FragmentSignupBinding
 
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentSignupBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
 }
