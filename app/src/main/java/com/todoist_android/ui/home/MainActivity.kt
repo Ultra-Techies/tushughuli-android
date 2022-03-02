2051f4a14c423ea749abbab65c631915b228d0f1
malcolm maima <malcolmmaima@gmail.com>
2022-03-02 16:19:31 +0300

    setup options on mainactivity

--- app/src/main/java/com/todoist_android/ui/home/MainActivity.kt
+++ app/src/main/java/com/todoist_android/ui/home/MainActivity.kt
@@ -1,9 +1,17 @@
 package com.todoist_android.ui.home
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import com.todoist_android.R
 import com.todoist_android.databinding.ActivityMainBinding
+import com.todoist_android.ui.auth.AuthActivity
+
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
@@ -13,9 +21,50 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        setSupportActionBar(binding.toolbar)
+        title = ""
+
         //Receiving data from the previous activity
         val userId = intent.getIntExtra("userId", 0)
 
         Toast.makeText(this, "Welcome back $userId", Toast.LENGTH_LONG).show()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.main_activity, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.profile -> {
+                startActivity(Intent(this, ProfileActivity::class.java))
+                true
+            }
+
+//            R.id.action_settings -> {
+//                startActivity(Intent(this, SettingsActivity::class.java))
+//                true
+//            }
+
+            R.id.action_logout -> {
+                val alertDialog = AlertDialog.Builder(this)
+                alertDialog.apply {
+                    setTitle("Logout?")
+                    setMessage("Are you sure you want to logout?")
+                    setPositiveButton("Yes") { _, _ ->
+                        //clear preferences then move to auth activity with clear flag
+
+                        finish()
+                    }
+                    setNegativeButton("No") { dialog, which ->
+                        dialog.dismiss()
+                    }
+                }.create().show()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
