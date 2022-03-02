72c0512150b85dd35fc92231efafbcb189505cd3
malcolm maima <malcolmmaima@gmail.com>
2022-03-02 13:31:51 +0300

    check if userid or access token is present, then login user automatically

--- app/src/main/java/com/todoist_android/data/repository/UserPreferences.kt
+++ app/src/main/java/com/todoist_android/data/repository/UserPreferences.kt
@@ -1,4 +1,4 @@
-package com.todoist_android.data
+package com.todoist_android.data.repository
 
 import android.content.Context
 import androidx.datastore.DataStore
@@ -6,27 +6,24 @@ import androidx.datastore.preferences.Preferences
 import androidx.datastore.preferences.createDataStore
 import androidx.datastore.preferences.edit
 import androidx.datastore.preferences.preferencesKey
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 
 class UserPreferences (context: Context) {
 
     private val applicationContext = context.applicationContext
-    private val dataStore: DataStore<Preferences>
+    private val dataStore: DataStore<Preferences> = applicationContext.createDataStore(
+        name = "todo_data_store"
+    )
 
-    init {
-        dataStore = applicationContext.createDataStore(
-            name = "user_preferences"
-        )
-    }
-
-    val token
+    val todoToken: Flow<String?>
         get() = dataStore.data.map { preferences ->
             preferences[TOKEN]
         }
 
-    suspend fun saveToken(token: String) {
+    suspend fun saveToken(todoToken: String) {
         dataStore.edit {
-            it[TOKEN] = token
+            it[TOKEN] = todoToken
         }
     }
 
