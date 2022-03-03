0a119547168d0e71dcbbf4c32a14edd57731e356
Njumbi <wambuinjumbi@gmail.com>
2022-03-03 19:52:07 +0300

    feat : Implement show keyboard function

--- app/src/main/java/com/todoist_android/ui/Utils.kt
+++ app/src/main/java/com/todoist_android/ui/Utils.kt
@@ -1,5 +1,10 @@
 package com.todoist_android.view
 
+import android.content.Context.INPUT_METHOD_SERVICE
+import android.view.inputmethod.InputMethodManager
+import android.view.inputmethod.InputMethodManager.SHOW_IMPLICIT
+import android.widget.EditText
+
 
 const val BASE_URL = "https://621ce943768a4e1020b93731.mockapi.io/api/v1/"
 
@@ -8,4 +13,14 @@ private const val emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+"
 fun validateEmail(email: String): Boolean {
     return email.matches(emailPattern.toRegex())
 }
+
+fun EditText.showKeyboard() {
+    if (requestFocus()) {
+        (context.getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager)
+            .showSoftInput(this, SHOW_IMPLICIT)
+        setSelection(text.length)
+    }
+}
+
+
 
