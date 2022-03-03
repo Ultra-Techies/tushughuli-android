a4aae5f97b4256943175232f3e1e0a04b26fe624
Njumbi <wambuinjumbi@gmail.com>
2022-03-03 19:46:45 +0300

    feature: add a button click listener

--- app/src/main/java/com/todoist_android/ui/home/MainActivity.kt
+++ app/src/main/java/com/todoist_android/ui/home/MainActivity.kt
@@ -1,9 +1,13 @@
 package com.todoist_android.ui.home
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context.INPUT_METHOD_SERVICE
 import android.os.Bundle
+import android.view.inputmethod.InputMethodManager
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.todoist_android.databinding.ActivityMainBinding
+import org.junit.jupiter.params.shadow.com.univocity.parsers.common.Context
+
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
@@ -16,6 +20,13 @@ class MainActivity : AppCompatActivity() {
         //Receiving data from the previous activity
         val userId = intent.getIntExtra("userId", 0)
 
+
+
         Toast.makeText(this, "Welcome back $userId", Toast.LENGTH_LONG).show()
+
+        binding.buttonNewTask.setOnClickListener {
+            val modalBottomSheet = BottomSheetFragment()
+            modalBottomSheet.show(supportFragmentManager, BottomSheetFragment.TAG)
+        }
     }
 }
