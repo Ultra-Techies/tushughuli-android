597c16c3aa3ec34a36ed83de6d7a3334db0c5844
Njumbi <wambuinjumbi@gmail.com>
2022-03-05 20:26:58 +0300

    feature: add function to add task

---
+++ app/src/main/java/com/todoist_android/ui/home/BottomSheetViewModel.kt
@@ -0,0 +1,22 @@
+package com.todoist_android.ui.home
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.todoist_android.data.repository.TasksRepo
+import com.todoist_android.data.requests.AddTaskRequest
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.shareIn
+import javax.inject.Inject
+
+@HiltViewModel
+class BottomSheetViewModel @Inject constructor(private val tasksRepo: TasksRepo) : ViewModel() {
+
+    fun addTasks(taskRequest: AddTaskRequest) = flow {
+        val response = tasksRepo.addTasks(taskRequest)
+        emit(response)
+    }.shareIn(viewModelScope, SharingStarted.Lazily)
+
+
+}
