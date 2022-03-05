139f8b61c3171eae7b37b706cc993cd5626eb80d
malcolm maima <malcolmmaima@gmail.com>
2022-03-05 13:47:21 +0300

    add stickyheader item decoration

---
+++ app/src/main/java/com/todoist_android/ui/home/StickyViewHolder.kt
@@ -0,0 +1,6 @@
+package com.todoist_android.ui.home
+
+import androidx.recyclerview.widget.RecyclerView
+import android.view.View
+
+open class StickyViewHolder(v: View) : RecyclerView.ViewHolder(v)
