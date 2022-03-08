f068c0f85018667d384dc20cd1bfa3b40b627a96
Njumbi <wambuinjumbi@gmail.com>
2022-03-08 23:58:04 +0300

    refactor; code clean up

--- app/src/main/java/com/todoist_android/data/network/APIAuthentication.kt
+++ app/src/main/java/com/todoist_android/data/network/APIAuthentication.kt
@@ -1,10 +1,5 @@
 package com.todoist_android.data.network
 
-import com.todoist_android.data.models.TodoModel
-import com.todoist_android.data.requests.AddTaskRequest
-import com.todoist_android.data.requests.DeleteTaskRequest
-import com.todoist_android.data.responses.AddTasksResponse
-import com.todoist_android.data.responses.DeleteTaskResponse
 import com.todoist_android.data.responses.LoginResponse
 import com.todoist_android.data.responses.SignupResponse
 import retrofit2.http.*
@@ -26,15 +21,5 @@ interface APIAuthentication {
         @Field("email") email: String,
         @Field("password") password: String
     ) : SignupResponse
-
-
-    @POST("/tasks")
-    suspend fun addTasks(@Body tasksRequest: AddTaskRequest): AddTasksResponse
-
-    @POST("/tasks")
-    suspend fun editTasks(@Body editTasksRequest: TodoModel): TodoModel
-
-    @DELETE("/tasks")
-    suspend fun deleteTasks(@Body deleteTaskRequest:DeleteTaskRequest): DeleteTaskResponse
 
 }
