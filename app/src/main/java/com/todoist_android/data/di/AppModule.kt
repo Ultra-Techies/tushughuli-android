e3ae0bf0f3335868fc48dc2aec765f55794e6b5c
Njumbi <wambuinjumbi@gmail.com>
2022-03-14 23:48:26 +0300

    feat: add scalars

--- app/src/main/java/com/todoist_android/data/di/AppModule.kt
+++ app/src/main/java/com/todoist_android/data/di/AppModule.kt
@@ -14,6 +14,7 @@ import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.converter.scalars.ScalarsConverterFactory
 import javax.inject.Singleton
 
 
@@ -39,6 +40,7 @@ object AppModule {
     @Singleton
     fun providesRetrofit(okHttpClient: OkHttpClient): Retrofit = Retrofit
         .Builder()
+        .addConverterFactory(ScalarsConverterFactory.create())
         .addConverterFactory(GsonConverterFactory.create())
         .addCallAdapterFactory(CoroutineCallAdapterFactory())
         .client(okHttpClient)
