b970d05be8663493768e795dba0070f0ef94110a
Njumbi <wambuinjumbi@gmail.com>
2022-03-15 23:48:36 +0300

    refactor: change tasks endpoints

--- app/src/main/java/com/todoist_android/data/network/TaskApi.kt
+++ app/src/main/java/com/todoist_android/data/network/TaskApi.kt
@@ -7,7 +7,7 @@ import com.todoist_android.data.responses.EditTaskResponse
 import com.todoist_android.data.responses.TasksResponse
 import retrofit2.http.Body
 import retrofit2.http.DELETE
-import retrofit2.http.GET
+import retrofit2.http.HTTP
 import retrofit2.http.POST
 import retrofit2.http.PUT
 import retrofit2.http.Path
@@ -16,24 +16,27 @@ interface TaskApi {
 
     @HTTP(method = "GET", path = "/api/task/{id}", hasBody = false) //todolist backend passes user id to get tasks
     suspend fun getTasks(
-        @Path("id")id: String
+        @Path("id")id: Int
     ) : TasksResponse
 
-    @POST("/api/task/add/{id}") //todolist backend passes user id to add task
-    suspend fun addTasks(@Body tasksRequest: AddTaskRequest, @Path("id") id: String): AddTasksResponse
+    @POST("api/task/add/{id}") //todolist backend passes user id to add task
+    suspend fun addTasks(
+        @Body taskRequest: AddTaskRequest,
+        @Path("id") id: Int): AddTasksResponse
 
-    @PUT("/api/task/update/{id}")
-    suspend fun editTasks(@Body editTasksRequest: TodoModel, @Path("id") id: String): TodoModel
+//    @PUT("/api/task/update/{id}")
+//    suspend fun editTasks(@Body editTasksRequest: TodoModel, @Path("id") id: String): TodoModel
 
-    @HTTP(method = "DELETE", path = "/api/task/delete/{id}", hasBody = true)
-    suspend fun deleteTasks(@Body deleteTaskRequest: TodoModel, @Path("id")id: String): TodoModel
-    @PUT("task/update/{id}")
+//    @HTTP(method = "DELETE", path = "/api/task/delete/{id}", hasBody = true)
+//    suspend fun deleteTasks(@Body deleteTaskRequest: TodoModel, @Path("id")id: String): TodoModel
+
+    @PUT("api/task/update/{id}")
     suspend fun editTasks(
         @Body editTaskRequest: EditTaskRequest,
         @Path("id") id: Int
     ): EditTaskResponse
 
-    @DELETE("task/delete/{id}")
+    @DELETE("api/task/delete/{id}")
     suspend fun deleteTasks(@Path("id") id: Int): String
 
 }
--- app/src/main/java/com/todoist_android/data/repository/TasksRepo.kt
+++ app/src/main/java/com/todoist_android/data/repository/TasksRepo.kt
@@ -1,7 +1,6 @@
 package com.todoist_android.data.repository
 
 
-import com.todoist_android.data.models.TodoModel
 import com.todoist_android.data.network.TaskApi
 import com.todoist_android.data.network.repository.BaseRepo
 import com.todoist_android.data.requests.AddTaskRequest
@@ -12,22 +11,18 @@ class TasksRepo@Inject constructor(
 private val taskApi: TaskApi
 ): BaseRepo()
 {
-    suspend fun addTasks(taskRequest: AddTaskRequest) = safeApiCall{
-        //taskApi.addTasks(taskRequest, taskRequest.id!!) //todolist backend requires id
-        taskApi.addTasks(taskRequest, taskRequest.id!!) //mock API
-    suspend fun addTasks(id: Int,taskRequest: AddTaskRequest) = safeApiCall{
-        taskApi.addTasks(taskRequest,id)
+
+    suspend fun addTasks(id: Int,taskRequest: AddTaskRequest) = safeApiCall {
+        taskApi.addTasks(taskRequest, id)
     }
 
     suspend fun editTasks(id: Int,editTasksRequest: EditTaskRequest) = safeApiCall {
         taskApi.editTasks(editTasksRequest,id)
-
     }
-
 
     suspend fun deleteTasks(id: Int) = safeApiCall { taskApi.deleteTasks(id) }
 
-    suspend fun getTasks(id: String) = safeApiCall {
+    suspend fun getTasks(id: Int) = safeApiCall {
         taskApi.getTasks(id)
     }
 }
