f740d0d72057456d7a953834722eff57dc534f84
Njumbi <wambuinjumbi@gmail.com>
2022-03-15 11:39:50 +0300

    refactor: refactor tests using mocck

--- app/src/test/java/com/todoist_android/data/repository/TasksRepoTest.kt
+++ app/src/test/java/com/todoist_android/data/repository/TasksRepoTest.kt
@@ -1,67 +1,174 @@
 package com.todoist_android.data.repository
 
-import androidx.arch.core.executor.testing.InstantTaskExecutorRule
-import com.google.common.truth.Truth
+import com.google.common.truth.Truth.assertThat
 import com.todoist_android.MainCoroutineRule
+import com.todoist_android.data.models.TodoModel
 import com.todoist_android.data.network.APIResource
 import com.todoist_android.data.network.TaskApi
 import com.todoist_android.data.requests.AddTaskRequest
 import com.todoist_android.data.responses.AddTasksResponse
-import kotlinx.coroutines.ExperimentalCoroutinesApi
+import io.mockk.coEvery
+import io.mockk.coVerify
+import io.mockk.mockk
 import kotlinx.coroutines.test.runTest
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.junit.runners.JUnit4
-import org.mockito.BDDMockito.given
-import org.mockito.Mock
-import org.mockito.Mockito
-import org.mockito.MockitoAnnotations
+
+
 
 @RunWith(JUnit4::class)
 class TasksRepoTest {
 
     lateinit var tasksRepo: TasksRepo
-
-    @Mock
     lateinit var taskApi: TaskApi
 
     @Before
     fun setUp() {
-        MockitoAnnotations.initMocks(this)
+        taskApi = mockk()
         tasksRepo = TasksRepoImpl(taskApi)
     }
 
     @get:Rule
     var mainCoroutineRule = MainCoroutineRule()
 
-    @OptIn(ExperimentalCoroutinesApi::class)
     @Test
-    fun addTasks() = runTest {
-        val testResponse = AddTasksResponse(
-            status = "created",
-            description = "sanple desc",
-            title = "Sample title",
-            id = 1,
-            due_date = "12/12/2022"
+    fun `add task, return success`() = runTest {
+        // given
+        val resp = AddTasksResponse(
+            description = "",
+            due_date = "",
+            id = 0,
+            status = "",
+            title = ""
         )
 
-        Mockito.`when`(taskApi.addTasks(AddTaskRequest())).thenReturn(
-            testResponse
-        )
+        // when
+        coEvery { taskApi.addTasks(any()) } returns resp
 
-        val res = tasksRepo.addTasks(AddTaskRequest())
-        Truth.assertThat(res).isEqualTo(APIResource.Success(testResponse))
+        // expect
+        val apiResponse = tasksRepo.addTasks(AddTaskRequest())
+
+        assertThat(apiResponse).isEqualTo(APIResource.Success(resp))
+        coVerify {
+            taskApi.addTasks(any())
+        }
     }
 
     @Test
-    fun addTasks_throwsException() = runTest {
-        given(taskApi.addTasks(AddTaskRequest())).willAnswer {
-            throw Exception("Ooops")
+    fun `add task, return fail`() = runTest {
+        // when
+        coEvery { taskApi.addTasks(any()) } throws Exception("Something went wrong")
+
+        // expect
+        val apiResponse = tasksRepo.addTasks(AddTaskRequest())
+
+        assertThat(apiResponse).isEqualTo(APIResource.Error(true, null, null))
+        coVerify {
+            taskApi.addTasks(any())
+        }
+    }
+
+    @Test
+    fun `edit task, return success`() = runTest{
+        val editTasksRequest = TodoModel(
+            title = "now",
+            description = "now now",
+            due_date = "2022-04-11 14:10:00",
+            id = "2",
+            status = "progress"
+
+        )
+        coEvery { taskApi.editTasks(any(),any()) }returns editTasksRequest
+
+        val apiResponse =tasksRepo.editTasks(TodoModel(
+            title = null,
+            description = null,
+            due_date = null,
+            id = "2",
+            status = null
+        ))
+        assertThat(apiResponse).isEqualTo(APIResource.Success(editTasksRequest))
+
+        coVerify {
+            taskApi.editTasks(any(),any())
+        }
+    }
+
+    @Test
+    fun `edit task, returns Error`()= runTest{
+
+        coEvery { taskApi.editTasks(any(),any()) }throws Exception("Something went wrong")
+
+        val apiResponse = tasksRepo.editTasks(
+            TodoModel(
+                title = null,
+                description = null,
+                due_date = null,
+                id = "2",
+                status = null
+            )
+        )
+        assertThat(apiResponse).isEqualTo(APIResource.Error(true,null,null))
+
+        coVerify { taskApi.editTasks(any(),any()) }
+
+    }
+
+    @Test
+    fun`deleteTasks, returns Success`() = runTest {
+    val deleteTaskRequest = TodoModel(
+        title = null,
+        description = null,
+        due_date = null,
+        id = "2",
+        status = null
+
+    )
+        coEvery { taskApi.deleteTasks(any(),any()) }returns deleteTaskRequest
+
+        val apiResponse = tasksRepo.deleteTasks(
+            TodoModel(
+                title = null,
+                description = null,
+                due_date = null,
+                id = "2",
+                status = null
+
+            )
+        )
+        assertThat(apiResponse).isEqualTo(APIResource.Success(deleteTaskRequest))
+
+        coVerify { taskApi.deleteTasks(any(),any()) }
+
+    }
+
+    @Test
+    fun `deleteTasks, returns error`()= runTest {
+
+        coEvery { taskApi.deleteTasks(any(),any()) }throws Exception("Something went wrong")
+
+        val apiResponse = tasksRepo.deleteTasks(
+            TodoModel(
+                title = null,
+                description = null,
+                due_date = null,
+                id = "2",
+                status = null
+
+            )
+        )
+        assertThat(apiResponse).isEqualTo(APIResource.Error(true,null,null))
+
+        coVerify {
+           taskApi.deleteTasks(any(),any())
         }
 
-        val res = tasksRepo.addTasks(AddTaskRequest(id = "2"))
-        Truth.assertThat(res).isEqualTo(APIResource.Error(true, null, null))
+
     }
+
+
+
 }
