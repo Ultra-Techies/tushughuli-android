f619be5c15b118ebee9ca7b6c1d1aefecffedb85
Njumbi <wambuinjumbi@gmail.com>
2022-03-17 11:17:31 +0300

    refactor: code clean up and refactoring

--- app/src/main/java/com/todoist_android/ui/home/BottomSheetViewModel.kt
+++ app/src/main/java/com/todoist_android/ui/home/BottomSheetViewModel.kt
@@ -2,21 +2,44 @@ package com.todoist_android.ui.home
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.todoist_android.data.network.APIResource
 import com.todoist_android.data.repository.TasksRepo
 import com.todoist_android.data.requests.AddTaskRequest
+import com.todoist_android.data.responses.AddTasksResponse
+import com.todoist_android.ui.parseErrors
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.shareIn
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class BottomSheetViewModel @Inject constructor(private val tasksRepo: TasksRepo) : ViewModel() {
 
-    fun addTasks(id:Int,taskRequest: AddTaskRequest) = flow {
-        val response = tasksRepo.addTasks(id,taskRequest)
-        emit(response)
-    }.shareIn(viewModelScope, SharingStarted.Lazily)
+    private val _addTaskResponse = MutableSharedFlow<AddTasksResponse>()
+    val addTasksResponse = _addTaskResponse.asSharedFlow()
 
+    private val _errorResponse = MutableSharedFlow<String>()
+    val errorResponse =_errorResponse.asSharedFlow()
+
+    fun addTasks(id: Int,taskRequest: AddTaskRequest) {
+    viewModelScope.launch {
+        val taskResponse = tasksRepo.addTasks(id,taskRequest)
+        when(taskResponse){
+            is APIResource.Success ->{
+                _addTaskResponse.emit(taskResponse.value)
+            }
+            is APIResource.Loading ->{
+
+            }
+            is APIResource.Error ->{
+                _errorResponse.emit(parseErrors(taskResponse))
+            }
+        }
+    }
+    }
 
 }
