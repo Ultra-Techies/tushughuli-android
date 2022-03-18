6175ed56ae0cdbaf80c3c5de3d7951c8158fdee7
Njumbi <wambuinjumbi@gmail.com>
2022-03-18 14:49:57 +0300

    refactor: code clean up

--- app/src/test/java/com/todoist_android/data/repository/FakeTasksRepo.kt
+++ app/src/test/java/com/todoist_android/data/repository/FakeTasksRepo.kt
@@ -3,49 +3,54 @@ package com.todoist_android.data.repository
 import com.todoist_android.data.models.TodoModel
 import com.todoist_android.data.network.APIResource
 import com.todoist_android.data.requests.AddTaskRequest
+import com.todoist_android.data.requests.EditTaskRequest
 import com.todoist_android.data.responses.AddTasksResponse
+import com.todoist_android.data.responses.EditTaskResponse
 import com.todoist_android.data.responses.TasksResponse
 import com.todoist_android.data.responses.TasksResponseItem
 
 class FakeTasksRepo : TasksRepo {
-    override suspend fun addTasks(taskRequest: AddTaskRequest): APIResource<AddTasksResponse> {
-        return APIResource.Success(
-            AddTasksResponse(
-                title = "test",
-                description = "test",
-                status = "created",
-                due_date = "12/12/2022",
-                id = 1
-            )
-        );
+    override suspend fun addTasks(
+        id: Int,
+        taskRequest: AddTaskRequest
+    ): APIResource<AddTasksResponse> {
+      return APIResource.Success( AddTasksResponse(
+            title = "test",
+            description = "test",
+            status = "created",
+            dueDate = "12/12/2022",
+            reminder = "11/12/2022",
+            id = 2
+        ))
     }
 
-    override suspend fun editTasks(editTasksRequest: TodoModel): APIResource<TodoModel> {
+    override suspend fun editTasks(
+        id: Int,
+        editTasksRequest: EditTaskRequest
+    ): APIResource<EditTaskResponse> {
         return APIResource.Success(
-            TodoModel(
-                title = "test",
+            EditTaskResponse(
                 description = "test",
+                dueDate = "2022-03-18 13:44:25",
+                id = 2,
+                reminder = "2022-03-17 13:44:25 ",
                 status = "created",
-                due_date = "12/12/2022",
-                id = "1"
+                title = "test"
+
             )
-        );
+        )
     }
 
-    override suspend fun deleteTasks(editTasksRequest: TodoModel): APIResource<TodoModel> {
-        return APIResource.Success(
-            TodoModel(
-                title = "test",
-                description = "test",
-                status = "created",
-                due_date = "12/12/2022",
-                id = "1"
-            )
-        );
+    override suspend fun deleteTasks(id: Int): APIResource<String> {
+  return APIResource.Success(
+      "Deleted successfully"
+  )
     }
 
-    override suspend fun getTasks(id: String): APIResource<TasksResponse> {
-        return APIResource.Success( TasksResponse() )
+    override suspend fun getTasks(id: Int): APIResource<TasksResponse> {
+       return APIResource.Success(
+           TasksResponse()
+       )
     }
 
 }
