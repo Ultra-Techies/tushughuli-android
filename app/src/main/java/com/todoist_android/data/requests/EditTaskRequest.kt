37cd06984db60e20157304ab1ea4bc74fb875307
Njumbi <wambuinjumbi@gmail.com>
2022-03-18 14:49:34 +0300

    refactor: code clean up

--- app/src/main/java/com/todoist_android/data/requests/EditTaskRequest.kt
+++ app/src/main/java/com/todoist_android/data/requests/EditTaskRequest.kt
@@ -2,7 +2,7 @@ package com.todoist_android.data.requests
 
 data class EditTaskRequest(
 //    val id: Int,
-    val description: String,
+    val description: String?,
     var dueDate: String,
     val reminder: String,
     var createdTime:String,
