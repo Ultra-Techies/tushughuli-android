7f84677092cdcf974fda36c3a9940352400a67d7
Njumbi <wambuinjumbi@gmail.com>
2022-03-18 14:47:56 +0300

    refactor: refactor code to make it testable

--- app/src/main/java/com/todoist_android/ui/home/BottomSheetViewModel.kt
+++ app/src/main/java/com/todoist_android/ui/home/BottomSheetViewModel.kt
@@ -9,10 +9,7 @@ import com.todoist_android.data.responses.AddTasksResponse
 import com.todoist_android.ui.parseErrors
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableSharedFlow
-import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.asSharedFlow
-import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.flow.shareIn
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -23,23 +20,23 @@ class BottomSheetViewModel @Inject constructor(private val tasksRepo: TasksRepo)
     val addTasksResponse = _addTaskResponse.asSharedFlow()
 
     private val _errorResponse = MutableSharedFlow<String>()
-    val errorResponse =_errorResponse.asSharedFlow()
+    val errorResponse = _errorResponse.asSharedFlow()
 
-    fun addTasks(id: Int,taskRequest: AddTaskRequest) {
-    viewModelScope.launch {
-        val taskResponse = tasksRepo.addTasks(id,taskRequest)
-        when(taskResponse){
-            is APIResource.Success ->{
-                _addTaskResponse.emit(taskResponse.value)
-            }
-            is APIResource.Loading ->{
+    fun addTasks(id: Int, taskRequest: AddTaskRequest) {
+        viewModelScope.launch {
+            val taskResponse = tasksRepo.addTasks(id, taskRequest)
+            when (taskResponse) {
+                is APIResource.Success -> {
+                    _addTaskResponse.emit(taskResponse.value)
+                }
+                is APIResource.Loading -> {
 
-            }
-            is APIResource.Error ->{
-                _errorResponse.emit(parseErrors(taskResponse))
+                }
+                is APIResource.Error -> {
+                    _errorResponse.emit(parseErrors(taskResponse))
+                }
             }
         }
-    }
     }
 
 }
